60ba217382e70e54330ef1248fedec6c8ee19c4e
Lakshya Kumar <153004209+predator-911@users.noreply.github.com>
2025-02-15 16:58:32 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if(username.equals("admin") && password.equals("1234")) {
+            out.println("Login Successful!");
+        } else {
+            out.println("Invalid Credentials!");
+        }
+    }
+}
